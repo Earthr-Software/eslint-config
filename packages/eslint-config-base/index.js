9c24994e758577fe3edb3052f75fb1520171a0da
Aydin Senturk <aydin@earthr.co>
2024-07-09 21:50:44 +0300

    eslint config base packages updated

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,25 +1,24 @@
 module.exports = {
   extends: [
-    "airbnb-base",
-    "plugin:eslint-comments/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
+    'airbnb-base',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   env: {
     jest: true,
     browser: true,
   },
-  plugins: ["import", "prettier", "jest", "eslint-comments"],
+  plugins: ['import', 'prettier', 'eslint-comments'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        printWidth: 100,
+        printWidth: 120,
         singleQuote: true,
-        trailingComma: "all",
+        trailingComma: 'all',
         bracketSpacing: true,
         tabWidth: 2,
       },
